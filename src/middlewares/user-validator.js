6d8d3b1ddb5e3a5bc5fc45cdb594319c8a853d52
Zodiacal0 <javierherrera5513@gmail.com>
2025-05-15 19:20:46 -0600

    feat(user): crud terminado

--- src/middlewares/user-validator.js
+++ src/middlewares/user-validator.js
@@ -1,7 +1,10 @@
-import { body , check } from "express-validator";
+import { body , check, param} from "express-validator";
 import { emailExist, userNameExist , uidExist} from "../helpers/db-validators.js";
 import { validationsFields } from "./validatorsFields.js";
 import {deleteFileOnError} from "./delete-file-error.js"
+import { validateJWT } from "./validate-token.js";
+import { hasRoles } from "./validate-role.js";
+import { catchErrors } from "./catch-errors.js";
 
 export const registerValidator = [
     body("name").not().isEmpty().withMessage("Name is required"),
@@ -21,24 +24,45 @@ export const loginValidator = [
 ];
 
 export const getUserByIdValidator = [
-    check("uid").isMongoId().withMessage("No es un ID válido"),
-    check("uid").custom(uidExist),
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    param("uid").custom(uidExist),
     validationsFields,
-    deleteFileOnError
-];
+    catchErrors
+]
 
 export const deleteUserValidator = [
-    check("uid").isMongoId().withMessage("No es un ID válido"),
-    check("uid").custom(uidExist),
+    validateJWT,
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB").custom(uidExist),
     validationsFields,
-    deleteFileOnError
-];
+    catchErrors
+]
 
 export const updatePasswordValidator = [
-    check("uid").isMongoId().withMessage("No es un Id de Mongo Válido"),
-    check("uid").custom(uidExist),
-    body("newPassword").isLength({min: 8}).withMessage("La contraseñ a debe tener 8 carácteres"),
+    validateJWT,
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB").custom(uidExist),
+    body("newPassword").isLength({min: 8}).isStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    }).withMessage("El password debe contener al menos 8 caracteres"),
     validationsFields,
-    deleteFileOnError
-];
+    catchErrors
+]
 
+export const updateUserValidator = [
+    validateJWT,
+    param("uid", "No es un ID válido").isMongoId().custom(uidExist),
+    validationsFields,
+    catchErrors
+]
+
+export const UpdateProfileValidator =[
+    validateJWT,
+    check("uid").notEmpty().isMongoId().withMessage("No es un ID válido de MongoDB").custom(uidExist),
+    validationsFields,
+    catchErrors
+];
