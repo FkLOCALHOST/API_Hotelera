e4cf5845d653a605f223b46a6d8f91869e28e5c7
Zodiacal0 <javierherrera5513@gmail.com>
2025-05-22 19:46:30 -0600

    feat(room): model

--- src/room/room.model.js
+++ src/room/room.model.js
@@ -1,6 +1,10 @@
 import { Schema, model } from 'mongoose';
 
 const roomSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
     number: {
         type: String,
         required: true,
